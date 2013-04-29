c227740ed4c128c472cc4b7f88d661cbe3d2ede6
Antonio <>
2013-04-29 00:10:02 -0300

    Implementação do pacote controller (Falta definir a estrutura completa da classe ContasController)

--- CaixaEletronico/src/br/com/mackenzie/caixaeletronico/controller/ContasController.java
+++ CaixaEletronico/src/br/com/mackenzie/caixaeletronico/controller/ContasController.java
@@ -3,11 +3,33 @@
  * and open the template in the editor.
  */
 package br.com.mackenzie.caixaeletronico.controller;
-
+import br.com.mackenzie.caixaeletronico.model.conta.*;
 /**
  *
- * @author 71306552
+ * @author Antonio
  */
 public class ContasController {
     
+    private Cartao cartao;
+    private Conta dadosConta;
+    
+    public void construct(Cartao cartao, String senha){
+        
+    }
+     public boolean validarConta(Cartao cartao){
+           return false;
+     }
+     
+     public void consultarExtrato(Cartao cartao, String dtInicio, String dtFim){
+         
+     }
+     public void transferirValor(Cartao cartao, float valor, int agencia, int conta){
+         
+     }
+     public void depositarValor(Cartao cartao, float valor, char tipoDeposito){
+         
+     }
+     public Conta consultarSaldo(Cartao cartao){
+       return dadosConta;
+     }
 }
